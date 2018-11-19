4217cd4f279bbca2e369a390b4feb4a2b14da11b
Kane Koo <kanekoo@gmail.com>
2018-11-19 19:41:05 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,5 @@
 # Write your #display_rainbow method here
+
+def display_rainbow(color_Array)
+  color_Initial_Array = ["R", "O", "G", "Y", "B", "I", "V"]
+  print "#{color_Initial_Array[0]}: #{color_Array[0], "
